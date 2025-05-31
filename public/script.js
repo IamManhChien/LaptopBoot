a11dbc6fc99309e5c1b6247406698bb50ea881f1
Chien <vumanhchien511203@gmail.com>
2025-06-01 04:09:27 +0700

    FrontEnd LaptopBoot

---
+++ public/script.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+    spaceBetween: 30,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints:{
+    1024:{
+        slidesPerView: 4
+    }
+  }
+
+});
+
+
+document.querySelectorAll('.product-grid-item').forEach(product => {
+  product.addEventListener('click', function () {
+    window.location.href = '/Product';
+  });
+});
