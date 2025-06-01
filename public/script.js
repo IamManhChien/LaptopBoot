25d34d6ca64cf3197a6aede2150418335fa0b9d7
Chien <vumanhchien511203@gmail.com>
2025-06-01 13:20:29 +0700

    Dieu chinh them phan chon san pham

--- public/script.js
+++ public/script.js
@@ -31,3 +31,23 @@ document.querySelectorAll('.slider-item').forEach(product => {
     window.location.href = '/Product';
   });
 });
+
+
+// ĐOẠN CODE SỬ DỤNG NẾU THÊM ID VÀO CÁC PHẦN
+// document.querySelectorAll('.product-grid-item').forEach(product => {
+//   product.addEventListener('click', function () {
+//     const id = product.getAttribute('data-id');
+//     if (id) {
+//       window.location.href = `/Product?id=${id}`;
+//     }
+//   });
+// });
+
+// document.querySelectorAll('.slider-item').forEach(product => {
+//   product.addEventListener('click', function () {
+//     const id = product.getAttribute('data-id');
+//     if (id) {
+//       window.location.href = `/Product?id=${id}`;
+//     }
+//   });
+// });
